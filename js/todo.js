706f0b415208d80af81ed373e6ac8886f386c1ee
Dex_Park <mns0327@naver.com>
2020-12-06 21:59:38 +0900

    toDOList 삭제 기능 구현

--- js/todo.js
+++ js/todo.js
@@ -4,18 +4,33 @@ const toDoForm = document.querySelector(".js-toDoForm"),
 
 const TODOS_LS = 'toDos';
 
-const toDos = [];
+let toDos = [];
+
+function deleteToDo(event) {
+    const btn = event.target;
+    const li = btn.parentNode;
+    toDoList.removeChild(li);
+    const cleanToDos = toDos.filter(function(toDo) {
+      return toDo.id !== parseInt(li.id);
+    });
+    toDos = cleanToDos;
+    saveToDos();
+}
 
 function saveToDos() {
     localStorage.setItem(TODOS_LS, JSON.stringify(toDos));
 }
 
+let idNum = 1;
+
 function paintToDo(text) {
     const li = document.createElement("li");
     const delBtn = document.createElement("button");
     delBtn.innerText = "❌";
+    delBtn.addEventListener("click", deleteToDo);
     const span = document.createElement("span");
-    const newId = toDos.length + 1;
+    const newId = idNum;
+    idNum++;
     span.innerText = text;
     li.appendChild(delBtn);
     li.appendChild(span);
